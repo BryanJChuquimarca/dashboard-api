adc172aa17016b7d2a409f0f6367317aed05f874
BryanJChuquimarca <bryanchuquimarcacastillo@gmail.com>
2026-02-04 19:48:34 +0100

    Actualizada coneccion a base de datos

--- dist/src/db-connection.js
+++ dist/src/db-connection.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.query = query;
 var pg_1 = require("pg");
 var pool = new pg_1.Pool({
-    user: 'postgres',
-    password: '0645',
-    host: 'localhost',
-    port: 5432, // default Postgres port
-    database: 'dashboard_db'
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    // user: 'postgres',
+    // password: '0645',
+    // host: 'localhost',
+    // port: 5432, // default Postgres port
+    // database: 'dashboard_db'
 });
 function query(text, params) {
     return pool.query(text, params);
 }
-;
--- src/db-connection.ts
+++ src/db-connection.ts
@@ -1,13 +1,15 @@
-import { Pool, QueryResult } from 'pg';
+import { Pool, QueryResult } from "pg";
 
 const pool = new Pool({
-  user: 'postgres',
-  password: '0645',
-  host: 'localhost',
-  port: 5432, // default Postgres port
-  database: 'dashboard_db'
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  // user: 'postgres',
+  // password: '0645',
+  // host: 'localhost',
+  // port: 5432, // default Postgres port
+  // database: 'dashboard_db'
 });
 
 export function query(text: string, params?: any[]): Promise<QueryResult> {
-    return pool.query(text, params);
-};
+  return pool.query(text, params);
+}
